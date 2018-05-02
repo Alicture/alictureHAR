82374da525b6c1336750a8ace2441947a2461516
Alicture <lzp123518@gmail.com>
2018-05-02 16:50:12 +0800

    updata blumix

--- server.py
+++ server.py
@@ -7,6 +7,31 @@ app = Flask(__name__)
 # app.register_blueprint(ck, url_prefix='/ck')
 # app.jinja_env.add_extension("chartkick.ext.charts")
 
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    print('Found VCAP_SERVICES')
+    if 'cloudantNoSQLDB' in vcap:
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+elif os.path.isfile('vcap-local.json'):
+    with open('vcap-local.json') as f:
+        vcap = json.load(f)
+        print('Found local VCAP_SERVICES')
+        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
 @app.route('/')
 
 @app.route('/index')
@@ -34,4 +59,4 @@ def randomtest():
 	return jsonify(data=[random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100),random.randint(-100,100)])
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0', port=port, debug=True)
